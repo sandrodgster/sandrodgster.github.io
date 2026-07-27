2d6db2580dbb508f4349f4380ac42a33c85e8d72
sandrodgster <sandrodgster@gmail.com>
2026-07-27 12:04:47 -0300

    Adiciona ano automático no footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log("Portfólio iniciado com sucesso!");
+
+const currentYearElement = document.querySelector("#current-year");
+
+if (currentYearElement) {
+    currentYearElement.textContent = new Date().getFullYear();
+}
